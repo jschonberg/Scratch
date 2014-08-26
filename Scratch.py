f95053d98d31ad7342b597c56fe4416ddd9df0ba
Jacob Schonberg <jacob.schonberg@gmail.com>
2014-08-25 23:37:42 -0400

    Add extension renaming capability

--- Scratch.py
+++ Scratch.py
@@ -3,14 +3,48 @@ import sublime_plugin
 import re
 import os
 
+def getPath():
+        s = sublime.load_settings("Scratch.sublime-settings")
+        save_path = s.get("save_path", "~/Documents/Scratch")
+        if save_path[0] == '~':
+            save_path = os.path.expanduser('~') + save_path[1:]
+        return save_path
+
+def getExtension():
+    s = sublime.load_settings("Scratch.sublime-settings")
+    save_extension = s.get("extension", ".scratch")
+    return save_extension
+
+
+
+class SyntaxPreSaveCommand(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        if not view.file_name().startswith(getPath()):
+            return
+
+        # If first line is a file extension:
+        infile_re = re.match(r"\.\w+$",view.substr(view.line(0)))
+        filename_re = re.match(r"(.+)(\.\w+)$",view.file_name())
+
+        if infile_re and filename_re and infile_re.group() != filename_re.group(2):
+            new_name = filename_re.group(1) + infile_re.group()
+            os.rename(view.file_name(), new_name)
+            with open(new_name, 'r') as fin:
+                data = fin.read().splitlines(True)
+            with open(new_name, 'w') as fout:
+                fout.writelines(data[1:])
+            sublime.active_window().run_command("close")
+            sublime.active_window().open_file(new_name)
+
+
 class ScratchCommand(sublime_plugin.WindowCommand):
     def run(self):
-        save_path = self._getPath()
-        save_extension = self._getExtension()
+        save_path = getPath()
+        save_extension = getExtension()
 
         #Ensure directory exists
         try:
-            os.mkdir(self._getPath())
+            os.mkdir(getPath())
         except OSError:
             pass
 
@@ -19,17 +53,9 @@ class ScratchCommand(sublime_plugin.WindowCommand):
         filename = save_path + "/" + str(new_int) + save_extension
         self.window.open_file(filename)
 
-    def _getPath(self):
-        s = sublime.load_settings("Scratch.sublime-settings")
-        save_path = s.get("save_path", "~/Documents/Scratch")
-        if save_path[0] == '~':
-            save_path = os.path.expanduser('~') + save_path[1:]
-        return save_path
+    
 
-    def _getExtension(self):
-        s = sublime.load_settings("Scratch.sublime-settings")
-        save_extension = s.get("extension", ".scratch")
-        return save_extension
+
 
     def _getNextInt(self, directory):
         """Return the next highest integer to be used for a filename in directory"""
